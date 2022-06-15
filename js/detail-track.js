e1d037a14855381da6f9dc01ef4181a72bd313ec
KevinDReid <kreid@udesa.edu.ar>
2022-06-15 10:23:46 -0300

    no hice cambios en detail-track

--- js/detail-track.js
+++ js/detail-track.js
@@ -1,8 +1,8 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
-let id = queryStringObj.get('id');
+queryStringObj.get('id');
 
-fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`)
+fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks')
     .then(function(response) {
         return response.json()
 })
@@ -13,18 +13,10 @@ fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`)
             let dataAlbum = data.data[i].album
             let dataArtist = data.data[i].artist
             let dataSong = data.data[i]
-
             let duration = dataSong.duration
-            function time (){
-                let minutes = Math.floor(duration/60);
-                let seconds = duration - minutes * 60 ;
-                if (seconds <= 9) {
-                    seconds = `0${seconds}`
-                }
-
-                return `${minutes}:${seconds}`
+            function time(duration){
+            duration/60
             }
-
 
             albumCover.innerHTML = `<img height="640" width="640" src="${dataAlbum.cover_big}" alt="${dataAlbum.title}">`
 
@@ -36,21 +28,24 @@ fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`)
                 <h2 class="cancion"> ${dataSong.title}</h3>
             </div>`
 
-            let artistName = document.querySelector ('.img')
-            artistName.innerHTML = `<h1>${dataArtist.name}</h1>`
-
             let fullTrackInfo = document.querySelector ('.divaside')
-            fullTrackInfo.innerHTML = `<div class="texto">
+            fullTrackInfo.innerHTML `<div class="texto">
             <h3 class="span1"> Canción</h3>
-            <span> ${dataSong.position}. </span><span> ${dataSong.title}</span>
-            <span class="tiempo"> ${time()}</span>
+            <span> 1. </span><span> ${dataSong.name}</span>
+            <span class="tiempo"> ${duration}</span>
             
-            <span class="triangulo"> <iframe style="border-radius:12px" src="${dataSong.preview}" width="100%" height="280" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe></span>
-            <audio src="${dataAlbum.cover_medium}"></audio>
+            <span class="triangulo"> <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/05IbhAKODbXn8og9uaUgpc?utm_source=generator" width="100%" height="280" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe></span>
+            <audio src="https://embed.music.apple.com/ar/album/permanence/1599768590?i=1599768594"></audio>
           
             <h3 class="span1"> Acerca del álbum</h3>
             <div class="div1"> 
-              <span class="span1">Artistas: </span><span class="desc"> ${dataArtist.name}</span>
+              <span class="span1">Artistas: </span><span class="desc"> David Guetta, Morten</span>
+            </div>
+            <div class="div2">
+              <span class="span1"> Fecha de lanzamiento: </span> <span class="desc"> 01-07-2022</span>
+            </div>
+            <div class="div3">
+              <span class="span1"> Genero:  </span><span class="desc"> Electronica</span>
             </div>
             <div class="div4">
               <span class="span2"> <a href="playlist.html" class="arc" >Añadir a mi playlist</a> </span>
