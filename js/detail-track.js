fc7e9a44cca8e22eb332b2bee72a4ef55aad3ddc
rana <fmoche@udesa.edu.ar>
2022-06-15 10:23:12 -0300

    aaaaaaaaaAAAAAAAAAAAA

--- js/detail-track.js
+++ js/detail-track.js
@@ -2,7 +2,7 @@ let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let id = queryStringObj.get('id');
 
-fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`)
+fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`)
     .then(function(response) {
         return response.json()
 })
@@ -52,6 +52,12 @@ fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`)
             <div class="div1"> 
               <span class="span1">Artistas: </span><span class="desc"> ${dataArtist.name}</span>
             </div>
+            <div class="div2">
+                        <span class="span1"> Fecha de lanzamiento: </span> <span class="desc"> ${dataAlbum.release_date}</span>
+                      </div>
+                      <div class="div3">
+                        <span class="span1"> Genero:  </span><span class="desc"> Electronica</span>
+                      </div>
             <div class="div4">
               <span class="span2"> <a href="playlist.html" class="arc" >Añadir a mi playlist</a> </span>
             </div>
